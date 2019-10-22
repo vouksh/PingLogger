3d6a5ea02f46297a9e2cba37fa73752cf3f9d19a
Jack Butler <vouksh@gmail.com>
2019-10-21 21:04:55 -0400

    Fixed a small formatting error.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("PingLogger")]
 [assembly: AssemblyDescription("Advanced multi-threaded ping logging application")]
-[assembly: AssemblyVersion("1.0.1.418")]
-[assembly: AssemblyFileVersion("1.0.1.418")]
+[assembly: AssemblyVersion("1.0.1.451")]
+[assembly: AssemblyFileVersion("1.0.1.451")]
