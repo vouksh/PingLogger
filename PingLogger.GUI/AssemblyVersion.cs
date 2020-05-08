7161872c755928d34b754c6b7d92676b5043614c
Jack Butler <vouksh@gmail.com>
2020-05-08 12:12:01 -0400

    Version bump

--- PingLogger.GUI/AssemblyVersion.cs
+++ PingLogger.GUI/AssemblyVersion.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("PingLogger")]
 [assembly: AssemblyDescription("Advanced multi-threaded ping logging application")]
-[assembly: AssemblyVersion("2.0.0.3865")]
-[assembly: AssemblyFileVersion("2.0.0.3865")]
+[assembly: AssemblyVersion("2.0.0.3866")]
+[assembly: AssemblyFileVersion("2.0.0.3866")]
