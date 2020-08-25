2fc0b73d06256db6c831dd52fd009153e0358a67
Jack Butler <vouksh@gmail.com>
2020-08-24 23:04:43 -0400

    Added check to make sure the hostname is valid before allowing user to add it.

--- Controls/AddHostDialog.xaml.cs
+++ Controls/AddHostDialog.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using PingLogger.GUI.Workers;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace PingLogger.GUI.Controls
 {
@@ -10,21 +15,59 @@ namespace PingLogger.GUI.Controls
 	public partial class AddHostDialog : Window
 	{
 		public ICommand CloseWindowCommand { get; set; }
+		private readonly SynchronizationContext syncCtx;
+		readonly DispatcherTimer Timer;
 		public AddHostDialog()
 		{
 			InitializeComponent();
 			CloseWindowCommand = new Command(Close);
+			syncCtx = SynchronizationContext.Current;
+			Timer = new DispatcherTimer
+			{
+				Interval = TimeSpan.FromMilliseconds(500),
+				IsEnabled = true
+			};
+			Timer.Tick += Timer_Tick;
+			Timer.Start();
 		}
+
+		private async void Timer_Tick(object sender, EventArgs e)
+		{
+			try
+			{
+				foreach (var ip in await Dns.GetHostAddressesAsync(hostNameBox.Text))
+				{
+					if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+					{
+						IsValidHost = true;
+
+						break;
+					}
+				}
+			}
+			catch (Exception)
+			{
+				IsValidHost = false;
+			}
+			syncCtx.Post(new SendOrPostCallback(o =>
+			{
+				AddBtn.IsEnabled = (bool)o;
+			}), IsValidHost);
+		}
+
+		public bool IsValidHost { get; set; } = false;
 
 		private void AddBtn_Click(object sender, RoutedEventArgs e)
 		{
 			(this.Owner as MainWindow).AddTab(hostNameBox.Text);
+			Timer.Stop();
 			this.DialogResult = true;
 			this.Close();
 		}
 
 		private void CancelBtn_Click(object sender, RoutedEventArgs e)
 		{
+			Timer.Stop();
 			this.DialogResult = false;
 			this.Close();
 		}
@@ -35,13 +78,21 @@ namespace PingLogger.GUI.Controls
 			{
 				(this.Owner as MainWindow).AddTab(hostNameBox.Text);
 				this.DialogResult = true;
+				Timer.Stop();
 				this.Close();
 			}
 			else if (e.Key == Key.Escape)
 			{
 				this.DialogResult = false;
+				Timer.Stop();
 				this.Close();
 			}
+		}
+
+		private void hostNameBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = false;
+			AddBtn.IsEnabled = false;
 		}
 	}
 }
