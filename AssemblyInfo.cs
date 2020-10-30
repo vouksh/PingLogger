90798ed774408b4bda747e93dcfe4d436493a1e6
Jack Butler <JButler@drbsystems.com>
2020-10-30 09:56:49 -0400

    Small tweaks to assembly info.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,9 +1,22 @@
 ﻿using System.Reflection;
 using System.Resources;
+using System.Runtime.InteropServices;
+
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
+[assembly: ComVisible(false)]
+
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("39E66F87-E17F-4311-A477-C5F47F7F7B1F")]
 
 [assembly: AssemblyTitle("PingLogger")]
 [assembly: AssemblyDescription("Advanced multi-threaded ping logging application")]
-[assembly: AssemblyVersion("2.2.0.2")]
-[assembly: AssemblyFileVersion("2.2.0.2")]
-
 [assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyVersion("2.2.0.10")]
+[assembly: AssemblyFileVersion("2.2.0.10")]
+
