83fea863d3192db7837e2d42f4c7bdc895d71029
Jack Butler <vouksh@gmail.com>
2022-04-17 23:11:50 -0400

    Changing app UI to Material design.

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PingLogger.Extensions;
+
+public static class StringExtensions
+{
+	public static string SplitCamelCase(this string input, string delimeter = " ")
+	{
+		return input.Any(char.IsUpper) ? string.Join(delimeter, Regex.Split(input, "(?<!^)(?=[A-Z])")) : input;
+	}
+
+}
